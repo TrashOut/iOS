8d91938ce300f44f88374fc35e2e705c96c47740
Peter Ertl <peter.ertl@thinkcreatix.com>
2020-05-19 10:59:53 +0200

    removed white lines on resized image edges

--- TrashOut/Image.swift
+++ TrashOut/Image.swift
@@ -21,13 +21,13 @@ public extension UIImage {
         // Figure out what our orientation is, and use that to form the rectangle
         var newSize: CGSize
         if(widthRatio > heightRatio) {
-            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+            newSize = CGSize(width: round(size.width * heightRatio), height: round(size.height * heightRatio))
         } else {
-            newSize = CGSize(width: size.width * widthRatio,  height: size.height * widthRatio)
+            newSize = CGSize(width: round(size.width * widthRatio),  height: round(size.height * widthRatio))
         }
         
         // This is the rect that we've calculated out and this is what is actually used below
-        let rect = CGRect(x: 0, y: 0, width: newSize.width, height: newSize.height)
+        let rect = CGRect(origin: .zero, size: newSize)
         
         // Actually do the resizing to the rect using the ImageContext stuff
         UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
